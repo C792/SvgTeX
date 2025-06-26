1224f61b379f4ec42634c696ab52d765526592b8
C792 <iansong10845@gmail.com>
2025-06-27 08:08:22 +0900

    Migration to Tauri

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const { invoke } = window.__TAURI__.core;
+
+function out() {
+  var element = document.getElementById("result")
+  const t = document.getElementById('mathinput').value;
+  // document.getElementById("result").innerText = m;
+  
+  element.innerText = "\\[" + t + "\\]";
+  renderMathInElement(document.body);
+}
+
+function dark() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
